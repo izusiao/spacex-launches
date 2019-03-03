fcd4f6866e0ff0ff8e15bff04268c861b4130ed5
Izabela Ozga <ozga.iza@gmail.com>
2019-03-03 14:37:50 +0100

    created client with apollo-client, layout, fixed graphQL schema for launch-success

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import "./App.css";
+import logo from "./logo.png";
+import Launches from "./components/Launches";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{
+              width: 300,
+              display: "block",
+              margin: "auto",
+              marginTop: 50
+            }}
+          />
+          <Launches />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
